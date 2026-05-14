5d027c62eaf3d307d1da9cc6107479e2aae676ff
EiEaintThwe <eieaintthwe9898@gmail.com>
2026-05-14 14:23:37 +0630

    first commit

---
+++ spa-react/src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+import { useState } from 'react';
+import Navbar from './assets/components/Navbar';
+import PostsList from './assets/components/PostsList';
+
+function App() {
+
+
+
+  let [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'First Post'
+    },
+    {
+      id: 2,
+      title: 'Second Post'
+    },
+    {
+      id: 3,
+      title: 'Third Post'
+    }
+  ]);
+
+
+
+  return (
+    <>
+      <Navbar />
+      <PostsList posts = {posts} />
+      
+    </>
+
+  );
+}
+
+export default App;
