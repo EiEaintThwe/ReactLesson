70360b0a0fc0fbe7741362b98584f5b6a0490451
EiEaintThwe <eieaintthwe9898@gmail.com>
2026-07-08 15:06:29 +0630

    fetch api function

---
+++ trip.mm/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import TripList from './components/TripList/index';
+
+function App() {
+  return (
+    <TripList />
+  );
+}
+
+export default App;
---
+++ trip.mm/src/components/TripList/index.js
@@ -0,0 +1,41 @@
+import React, { useCallback,useEffect, useState } from 'react'
+
+export default function Index() {
+
+    let [trips, setTrips] = useState([]);
+    let [url, setUrl] = useState('http://localhost:3001/trips');
+
+    let fetchTrips = useCallback(()=>{
+         fetch(url)
+            .then(res => res.json())
+            .then(data => {
+               setTrips(data);
+            })
+    },[url]);
+
+    useEffect(() => {
+       fetchTrips()
+    }, [fetchTrips]);
+    
+    console.log(trips);
+
+
+    return (
+        <div>
+            <h1>Ready to go?</h1>
+
+            <button onClick={()=>setUrl('http://localhost:3001/trips')}>All</button>
+            <button onClick={()=>setUrl('http://localhost:3001/trips?location=Myanmar')}>Trips in Myanmar</button>
+
+
+            <ul>
+               {trips.map(trip => (
+                 <li key={trip.id}>
+                    <h3>{trip.name}</h3>
+                    <p>Price - {trip.price} mmk</p>
+                </li>
+               ))}
+            </ul>
+        </div>
+    )
+}
